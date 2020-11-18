26a2ec6c2251090e92e30fb2ca49f1c0b1e16029
yangnibal <wonjun9670@naver.com>
2020-11-18 18:52:39 +0900

    update

--- src/pages/main/components/FamilySite.js
+++ src/pages/main/components/FamilySite.js
@@ -5,7 +5,7 @@ const FamilySite = () => {
     return(
         <div className={styles.container}>
             <div className={styles.title_wrapper}>
-                <div className={styles.title}>패밀리 사이트</div>
+                <div className={styles.title}>관련 사이트</div>
             </div>
             
             <div className={styles.img_wrapper}>
